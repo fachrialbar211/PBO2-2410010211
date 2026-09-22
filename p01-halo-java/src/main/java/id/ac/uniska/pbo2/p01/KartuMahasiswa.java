bc05fbf6567bf9bc52f4943bad2adc9d7493e9d0
fachri albar <2410010211fachrialbar000@gmail.com>
2026-09-22 21:16:18 +0700

    menambahkan kartu mahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author Admin
+ */
+public class KartuMahasiswa {
+    public static void main(String[] args) {
+        String nama;
+        long npm;
+        int smstr;
+        String prodi;
+        String alasan;
+        
+        nama = "Fachri Albar";
+        npm = 2410010211L;
+        smstr = 5;
+        prodi = "Teknik Informatika";
+        alasan = "mendalami minat & bakat";
+        System.out.println("===================");
+        System.out.println("KARTU MAHASISWA PBO2");
+        System.out.println("===================");
+        System.out.println("NAMA    : " + nama);
+        System.out.println("NPM     : " + npm);
+        System.out.println("SEMESTER: " + smstr);
+        System.out.println("PRODI   : " + prodi);
+        System.out.println("ALASAN  : " + alasan);
+        System.out.println("===================");
+      
+        
+        
+    }
+    
+}
